49a2638d65e7036a070576912c6fb7188870db2b
Jay <leejay100@gmail.com>
2016-11-29 06:12:56 -0600

    Add WooCommerce and Magento to framework list.

--- web/controllers/application_controller.ex
+++ web/controllers/application_controller.ex
@@ -9,10 +9,10 @@ defmodule Themelook.Application do
 
   def call(conn, repo) do
     cat_query = from c in Category,
-      where: not c.name in ["HTML", "Shopify", "Wordpress", "Opencart", "Prestashop"],
+      where: not c.name in ["HTML", "Shopify", "Wordpress", "Opencart", "Prestashop", "WooCommerce", "Magento"],
       order_by: c.name
     framework_query = from c in Category,
-      where: c.name in ["HTML", "Shopify", "Wordpress", "Opencart", "Prestashop"],
+      where: c.name in ["HTML", "Shopify", "Wordpress", "Opencart", "Prestashop", "WooCommerce", "Magento"],
       order_by: c.name
     home_categories = Repo.all(cat_query)
     frameworks = Repo.all(framework_query)
